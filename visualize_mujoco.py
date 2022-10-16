0906cb490b783b04d28a1ccf96525b15118b15c7
amir_karimi96 <amir.karimi96@gmail.com>
2022-10-16 13:05:12 -0400

    visualizations changes

--- visualize_mujoco.py
+++ visualize_mujoco.py
@@ -1,4 +1,4 @@
-from env_wrapper import  D2C, Env_test, CT_pendulum, CT_pendulum_sparse,CT_mountain_car,CT_close_drawer
+from env_wrapper import  * #D2C, Env_test, CT_pendulum, CT_pendulum_sparse,CT_mountain_car,CT_close_drawer
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.animation as animation
@@ -16,10 +16,15 @@ import numpy as np
 from sub_policies import sub_policy
 from agents import *
 from multiprocessing import Queue
+
 try:
     import rlbench.gym
 except:
     pass
+
+from mujoco_py import GlfwContext
+# GlfwContext(offscreen=True)
+
 torch.set_printoptions(precision=10)
 norm = mpl.colors.Normalize(vmin=-2, vmax=2)
 cmap = cm.jet
@@ -43,13 +48,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--ID',default=0,type=int, help='param ID ')
 parser.add_argument('--config',default='0',type=str, help='config file name')
 parser.add_argument('--result_path',default='0',type=str, help='result folder path')
-parser.add_argument('--load_model', default=None,type=str, help='model')
+parser.add_argument('--model_path', default='0',type=str, help='model path')
 
 args = parser.parse_args()
 
 
 run_ID = args.ID
-load_model = args.load_model
+model_path = args.model_path
 cfg_path = args.config
 torch.manual_seed(run_ID * 1000) 
 result_path = args.result_path
@@ -57,7 +62,7 @@ with open(cfg_path) as file:
     config = yaml.full_load(file)
 
 param = config['param']
-param['env_dt'] = 0.05
+# param['env_dt'] = 0.02
 config_name = 'config_{}'.format(config['param_ID'])
 env = globals()[param['env']](dt=param['env_dt'])
 # env.seed(10)
@@ -78,14 +83,14 @@ if param['log_level'] >= 1:
 if param['async'] == False:
     config['writer'] = writer
     
-if param['async']:
-    agent = globals()[param['agent']](config, RB_sample_queue, agent_info_queue)
-else:
-    agent = globals()[param['agent']](config)
-agent.load_actor('0_2550.model')
+# if param['async']:
+agent = globals()[param['agent']](config, RB_sample_queue, agent_info_queue)
+# else:
+#     agent = globals()[param['agent']](config)
+agent.load_actor(model_path)
 low_level_function = sub_policy(low_level_function_choice = param['low_level_function'], low_level_action_dim = action_dim, n_features=int(param['z_dim']//action_dim)).low_level_function
 num_ep = 500
-continuous_env = D2C(discrete_env= env, low_level_funciton = low_level_function, rho = agent.rho, precise=True, eval_mode=True)
+continuous_env = D2C(discrete_env= env, low_level_funciton = low_level_function, rho = agent.rho, precise=False, eval_mode=True)
 
 
 def add_durations(im, x,d,color, add_line=False):
@@ -103,7 +108,7 @@ def add_durations(im, x,d,color, add_line=False):
     return im
 
 def get_concat_v(im1, im2):
-    dst = Image.new('RGB', (im1.width, im1.height + im2.height))
+    dst = Image.new('RGB', (im2.width, im1.height + im2.height))
     dst.paste(im1, (0, 0))
     dst.paste(im2, (0, im1.height))
     return dst
@@ -128,12 +133,13 @@ def evaluate(config):
     program_real_time = 0
     start_time = time.time()
     # max_experiment_time = param['max_experiment_time']
-    max_experiment_time = 10
+    max_experiment_time = 20
     
     z_dim = param['z_dim']
     durations = []
     new_movements = []
     actions = []
+    actions_low = []
     frames = []
     while environment_real_time < max_experiment_time:
         state = continuous_env.reset()
@@ -144,14 +150,14 @@ def evaluate(config):
         ds = []
         colors = ['red', 'blue']
         x = 0
-        while True :
-
+        while environment_real_time < max_experiment_time :
+            print(environment_real_time / max_experiment_time)
             # get actor net outputs based on current state and previous option
             predictions = agent.actor_network(S)
             
             # sample z and duration
-            z, _ = agent.get_action_z(predictions['z_mu'], predictions['z_sigma'])
-            D, _ = agent.get_duration(predictions['D_mu'], predictions['D_sigma'])
+            z, _ = agent.get_action_z(predictions['z_mu'], 0.1 * predictions['z_sigma'])
+            D, _ = agent.get_duration(predictions['D_mu'], 0.1 * predictions['D_sigma'])
             
             # set step duration to duration
             d = D.detach().numpy()[0]
@@ -171,12 +177,14 @@ def evaluate(config):
                 delay(d, environment_real_time=environment_real_time, program_real_time=program_real_time)
             
             # set elapsed time
-            environment_real_time += d
-            agent.real_t += d
-            
+            environment_real_time += np.array(info['durations']).sum()
+            agent.real_t += np.array(info['durations']).sum()
+            agent.real_t = param['env_dt'] * round(agent.real_t / param['env_dt'])
             frames.extend(info['frames'])
             undiscounted_rewards.extend(info['rewards'])
             durations.extend(info['durations'])
+            actions_low.extend(info['actions'])
+            # print(info['actions'])
             # RB_list.append([s, sp, z, R, done, D, T, tau, agent.real_t])
             # print('R: ', R)
             agent.total_steps += 1
@@ -190,9 +198,9 @@ def evaluate(config):
                 
             break
 
-    return frames, durations, new_movements, actions
+    return frames, durations, new_movements, actions, actions_low
 
-frames , durations, new_movements, actions = evaluate(config)
+frames , durations, new_movements, actions, actions_low = evaluate(config)
 x = np.float32(0.)
 n = -1
 c = ['red', 'blue']
@@ -201,20 +209,27 @@ im_d = PIL.Image.new(mode = "RGB", size = (1000, 200),
                            color = (255, 255, 255))
 # print(len(frames), len(durations))
 j = 0
-print(new_movements, durations, len(frames))
+print(new_movements, len(frames))
 for i,f in enumerate(frames):
     # print(x, new_movements[n])
     if n < (len(new_movements)-1):
-        if np.abs(x - new_movements[n+1]) < 0.001:
+        if np.abs(x - new_movements[n+1]) < 0.0001:
             n+=1
             add_line = True
     
     # print(add_line, x, new_movements[n+1])
-    im_d = add_durations(im_d,x*100,durations[i]*100 ,color=m.to_rgba(actions[n]), add_line=add_line)
+    print(x)
+    # im_d = add_durations(im_d,int(x*200),int(durations[i]*200) ,color=m.to_rgba(actions[n]), add_line=add_line)
+    im_d = add_durations(im_d,int(x*400),int(durations[i]*400) ,color=m.to_rgba(actions_low[i]), add_line=add_line)
+
     add_line = False
     x = x+durations[i]
-    print(f.shape)
-    frames2.append(get_concat_v(Image.fromarray((f * 255).astype(np.uint8)), im_d))
-    print(i)
+    x = param['env_dt'] * round(x / param['env_dt'])
+    # print(f.shape)
+    # print(f)
+    # img = Image.fromarray((f * 255).astype(np.uint8))
+    img = Image.fromarray(f)
+    frames2.append(get_concat_v(img, im_d))
+    # print(i)
 
-imageio.mimwrite(os.path.join('./videos/', 'random_agent.mp4'), frames2, fps=10)
+imageio.mimwrite(os.path.join('./videos/', 'random_agent.mp4'), frames2, fps=1/param['env_dt'])
